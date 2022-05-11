58b9343332ad5445107687ead0e60ebd1b8e607f
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-05-11 18:32:22 +0200

    Create sendNewGame, sendScore, getScore functions

---
+++ src/modules/api.js
@@ -0,0 +1,28 @@
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+
+export const sendNewGame = async (body) => fetch(url, {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => json.result)
+  .catch((error) => { throw error; });
+
+export const sendScore = async (gameId, body) => fetch(`${url + gameId}/scores/`, {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => json.result)
+  .catch((error) => { throw error; });
+
+export const getScoresApi = async (gameId) => fetch(`${url + gameId}/scores/`)
+  .then((response) => response.json())
+  .then((json) => json.result)
+  .catch((error) => { throw error; });
